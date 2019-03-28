b544d7adab5a930d7dd63ee11c663d96e4856a71
adam mckenzie <adammckenzie@adams-MacBook-Pro-2.local>
2019-03-28 08:56:14 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+
+      this.state = {
+        person: data,
+        index: 0
+      };  
+    }
+    handleClick =() => {
+      this.setState({ index: this.state.index + 1})
+    }
+    handlePrevious = () => {
+      if(this.state.index > 0) {
+        return this.setState({ index: this.state.index - 1})
+      }
+    }
+    nextClick = () => {
+      this.setState({index: (this.state.index + 1) % 25})
+    }
+  
   render() {
+    let person = this.state.person[this.state.index]
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+      <div>
+        <header className="header">
+        <h1 className='Home'>Home</h1>
         </header>
+        <div className="center-box">
+        <h1 className='person'>{person.name.first} {person.name.last}</h1>
+        
+        <h1><strong>from: </strong> {person. city} {person. country}</h1>
+        <h1><strong>Job title: </strong>{person.title}</h1>
+        <h1><strong>Employer: </strong>{person.employer}</h1>
+        <h1><strong>Favorite Movies: </strong></h1>
+        <ul>
+        <li>{person.favoriteMovies[0]}</li>
+        <li>{person.favoriteMovies[1]}</li>
+        <li>{person.favoriteMovies[2]}</li>
+        </ul>
+        <button onClick={this.handleClick} className='number'> {person.id} /25 </button>
+        </div>
+        <button onClick={this.handlePrevious} className='previous'>{ '< Previous' }</button>
+        <button className='edit'>Edit</button>
+        <button className='delete'>Delete</button>
+        <button className='new'>New</button>
+        <button onClick={this.nextClick} className='next'>Next ></button>
       </div>
     );
   }
---
+++ src/page-number.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class pagenumber extends Component {
+constructor(props){
+    super(props);
+
+    this.state = {
+        index: 0
+    };
+}
+
+render(){
+    return(
+        <h3>1/25</h3>
+    )
+}
+}
